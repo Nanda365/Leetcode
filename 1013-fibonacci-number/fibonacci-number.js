ba733b54c48ccdd50cb52244fc722fed5adda209
Kosuri Yehonanda Sai <152471307+Nanda365@users.noreply.github.com>
2026-05-29 00:28:14 +0530

    Time: 41 ms (83.13%) | Memory: 54.1 MB (27.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,12 +3,21 @@
  * @return {number}
  */
 var fib = function(n) {
-   if(n===0){
-    return 0
-   }
-   if(n===1){
-    return 1
-   }
 
-   return fib(n-1)+fib(n-2);
+    if(n === 0){
+        return 0;
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for(let i = 2; i <= n; i++){
+
+        let temp = a + b;
+
+        a = b;
+        b = temp;
+    }
+
+    return b;
 };
