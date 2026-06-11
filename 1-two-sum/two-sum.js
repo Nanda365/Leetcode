91328e950852913bde1224ac3050c197cbf00723
Kosuri Yehonanda Sai <152471307+Nanda365@users.noreply.github.com>
2026-06-11 16:29:48 +0530

    Time: 2 ms (66.88%) | Memory: 54.8 MB (52.27%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,10 +6,9 @@
 var twoSum = function(nums, target) {
     let map = {};
     for(let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-
-        if(map[complement] !== undefined) {
-            return [map[complement], i];
+        let newtarget = target - nums[i];
+        if(map[newtarget] !== undefined) {
+            return [map[newtarget], i];
         }
         map[nums[i]] = i;
     }
