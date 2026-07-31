5c03eb1ae57f9047cbd58fafa6b9c9fa5d0d0648
Kosuri Yehonanda Sai <152471307+Nanda365@users.noreply.github.com>
2026-07-31 19:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.44%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,7 +3,7 @@ class Solution:
         pre = ""
         for i in range(len(strs[0])):
             ch = strs[0][i]
-            
+
             for word in strs:
                 if i >= len(word) or word[i] != ch:
                     return pre
